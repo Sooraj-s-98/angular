5db8a6ad9f7a73a38adbb0e6bee5467d330cc8c7
sooraj_s <sooraj1998ayiroor@gmail.com>
2020-07-31 15:08:34 +0530

    Basic Service

--- src/app/shared/dishes.ts
+++ src/app/shared/dishes.ts
@@ -15,32 +15,32 @@ export const DISHES: Dish[] = [
             {
                 rating: 5,
                 comment: 'Imagine all the eatables, living in conFusion!',
-                author: 'John Lemon',
-                date: '2012-10-16T17:57:28.556094Z'
+                author: 'sooraj s',
+                date: '2019-10-16T17:57:28.556094Z'
             },
             {
                 rating: 4,
                 comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-                author: 'Paul McVites',
-                date: '2014-09-05T17:57:28.556094Z'
+                author: 'Akhil s',
+                date: '2019-09-05T17:57:28.556094Z'
             },
             {
                 rating: 3,
                 comment: 'Eat it, just eat it!',
-                author: 'Michael Jaikishan',
-                date: '2015-02-13T17:57:28.556094Z'
+                author: 'Rahul',
+                date: '2019-02-13T17:57:28.556094Z'
             },
             {
                 rating: 4,
                 comment: 'Ultimate, Reaching for the stars!',
-                author: 'Ringo Starry',
-                date: '2013-12-02T17:57:28.556094Z'
+                author: 'pranav ',
+                date: '2019-12-02T17:57:28.556094Z'
             },
             {
                 rating: 2,
                 comment: 'It\'s your birthday, we\'re gonna party!',
-                author: '25 Cent',
-                date: '2011-12-02T17:57:28.556094Z'
+                author: 'Akshay',
+                date: '2019-12-02T17:57:28.556094Z'
             }
         ]
     },
@@ -57,32 +57,32 @@ export const DISHES: Dish[] = [
             {
                 rating: 5,
                 comment: 'Imagine all the eatables, living in conFusion!',
-                author: 'John Lemon',
-                date: '2012-10-16T17:57:28.556094Z'
+                author: 'sooraj s',
+                date: '2019-10-16T17:57:28.556094Z'
             },
             {
                 rating: 4,
                 comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-                author: 'Paul McVites',
-                date: '2014-09-05T17:57:28.556094Z'
+                author: 'Ajay ',
+                date: '2019-09-05T17:57:28.556094Z'
             },
             {
                 rating: 3,
                 comment: 'Eat it, just eat it!',
-                author: 'Michael Jaikishan',
-                date: '2015-02-13T17:57:28.556094Z'
+                author: 'Adithya',
+                date: '2019-02-13T17:57:28.556094Z'
             },
             {
                 rating: 4,
                 comment: 'Ultimate, Reaching for the stars!',
-                author: 'Ringo Starry',
-                date: '2013-12-02T17:57:28.556094Z'
+                author: 'Amar',
+                date: '2019-12-02T17:57:28.556094Z'
             },
             {
                 rating: 2,
                 comment: 'It\'s your birthday, we\'re gonna party!',
-                author: '25 Cent',
-                date: '2011-12-02T17:57:28.556094Z'
+                author: 'Anoop',
+                date: '2019-12-02T17:57:28.556094Z'
             }
         ]
     },
@@ -99,32 +99,32 @@ export const DISHES: Dish[] = [
             {
                 rating: 5,
                 comment: 'Imagine all the eatables, living in conFusion!',
-                author: 'John Lemon',
-                date: '2012-10-16T17:57:28.556094Z'
+                author: 'Ajold',
+                date: '2019-10-16T17:57:28.556094Z'
             },
             {
                 rating: 4,
                 comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-                author: 'Paul McVites',
-                date: '2014-09-05T17:57:28.556094Z'
+                author: 'Aswin',
+                date: '2019-09-05T17:57:28.556094Z'
             },
             {
                 rating: 3,
                 comment: 'Eat it, just eat it!',
-                author: 'Michael Jaikishan',
-                date: '2015-02-13T17:57:28.556094Z'
+                author: 'Ajay',
+                date: '2019-02-13T17:57:28.556094Z'
             },
             {
                 rating: 4,
                 comment: 'Ultimate, Reaching for the stars!',
-                author: 'Ringo Starry',
-                date: '2013-12-02T17:57:28.556094Z'
+                author: 'Akhil',
+                date: '2019-12-02T17:57:28.556094Z'
             },
             {
                 rating: 2,
                 comment: 'It\'s your birthday, we\'re gonna party!',
-                author: '25 Cent',
-                date: '2011-12-02T17:57:28.556094Z'
+                author: 'Meenu',
+                date: '2019-12-02T17:57:28.556094Z'
             }
         ]
     },
@@ -141,32 +141,32 @@ export const DISHES: Dish[] = [
             {
                 rating: 5,
                 comment: 'Imagine all the eatables, living in conFusion!',
-                author: 'John Lemon',
-                date: '2012-10-16T17:57:28.556094Z'
+                author: 'Anoop',
+                date: '2019-10-16T17:57:28.556094Z'
             },
             {
                 rating: 4,
                 comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-                author: 'Paul McVites',
-                date: '2014-09-05T17:57:28.556094Z'
+                author: 'shaji',
+                date: '2019-09-05T17:57:28.556094Z'
             },
             {
                 rating: 3,
                 comment: 'Eat it, just eat it!',
-                author: 'Michael Jaikishan',
-                date: '2015-02-13T17:57:28.556094Z'
+                author: 'Aswin',
+                date: '2019-02-13T17:57:28.556094Z'
             },
             {
                 rating: 4,
                 comment: 'Ultimate, Reaching for the stars!',
-                author: 'Ringo Starry',
-                date: '2013-12-02T17:57:28.556094Z'
+                author: 'Ajay',
+                date: '2019-12-02T17:57:28.556094Z'
             },
             {
                 rating: 2,
                 comment: 'It\'s your birthday, we\'re gonna party!',
-                author: '25 Cent',
-                date: '2011-12-02T17:57:28.556094Z'
+                author: 'sooraj s',
+                date: '2019-12-02T17:57:28.556094Z'
             }
         ]
     }
