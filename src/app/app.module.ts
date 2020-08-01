3dbf9dabce4f88acfde94f2bf307568450d9bfa6
sooraj_s <sooraj1998ayiroor@gmail.com>
2020-08-01 17:11:30 +0530

    about component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import {LeaderService} from './services/leader.service';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -47,7 +48,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   providers: [
     DishService,
-    PromotionService
+    PromotionService,
+    LeaderService
   ],
   bootstrap: [AppComponent]
 })
